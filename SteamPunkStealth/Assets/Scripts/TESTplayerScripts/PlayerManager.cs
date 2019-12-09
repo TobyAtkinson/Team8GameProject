b996df7e2d7bc810e0610c6a1f7eaf280d43898c
AdrianTofan <adrianvasile.tofan@gmail.com>
2019-12-09 04:27:25 +0000

    added a new scene for AI testing

---
+++ SteamPunkStealth/Assets/Scripts/TESTplayerScripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region SingleTon
+
+    public static PlayerManager instance;
+
+
+     void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
