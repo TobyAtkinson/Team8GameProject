937a5273564f3d8453eca3db7d9700e7cee7e6ab
AdrianTofan <adrianvasile.tofan@gmail.com>
2020-01-26 00:02:35 +0000

    +Added IDLE roaming for the blind Enemy.

--- SteamPunkStealth/Assets/Scripts/blindAIscripts/blindEnemyController.cs
+++ SteamPunkStealth/Assets/Scripts/blindAIscripts/blindEnemyController.cs
@@ -5,16 +5,31 @@ using UnityEngine.AI;
 
 public class blindEnemyController : MonoBehaviour
 {
-    public float lookRadius = 30f;
 
+    public float lookRadius = 30f;
     Transform target;
     public NavMeshAgent agent;
+    Vector3 location;
+    Vector3 min;
+    Vector3 max;
+    Transform wall;
+    int radius;
+    int timer = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
+        wall = PlayerManager.instance.wall.transform;
         agent = GetComponent<NavMeshAgent>();
+        min.x = 1;
+        min.y = 0;
+        min.z = 1;
+        max.x = 25;
+        max.y = 0;
+        max.z = 25;
+
     }
 
     // Update is called once per frame
@@ -23,6 +38,10 @@ public class blindEnemyController : MonoBehaviour
         float distance = Vector3.Distance(target.position, transform.position);
 
         playerCrouching();
+        if (timer % 75 == 0)
+        {
+            idleRoam();
+        }
 
         if (distance <= lookRadius)
             agent.SetDestination(target.position);
@@ -31,6 +50,7 @@ public class blindEnemyController : MonoBehaviour
         {
             facePlayer();
         }
+        timer++;
     }
 
     void playerCrouching()
@@ -50,8 +70,42 @@ public class blindEnemyController : MonoBehaviour
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 7f);
 
+    }
+
+    void idleRoam()
+    {
+        /*
+                if (wall.position != location)
+                {
+                    MathUtilities.Random(ref location, min, max);
+                    lookRoam(location);
+
+                    agent.SetDestination(location);
+
+                }
+                */
+
+        
+        radius = Random.Range(1, 15);
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        lookRoam(ref randomDirection);
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, radius, 1);
+        Vector3 finalPos = hit.position;
+        agent.SetDestination(finalPos);
+        
+        
+    }
+
+    void lookRoam(ref Vector3 randomDirection)
+    {
+     //   MathUtilities.Random(ref location, min, max);
+     //  Vector3  roamDirection = (randomDirection - transform.position).normalized;
+        Quaternion lookRotationRoam = Quaternion.LookRotation(new Vector3(randomDirection.x, 0, randomDirection.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotationRoam, Time.deltaTime * 7f);
     }
 
     private void OnDrawGizmosSelected()
