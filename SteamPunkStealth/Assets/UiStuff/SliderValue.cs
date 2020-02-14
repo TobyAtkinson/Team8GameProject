7c4cdbd86891b52285b1b7f6c50d7a7eccb97474
Edvilio <57681033+Edvilio@users.noreply.github.com>
2020-02-14 11:59:25 +0000

    Added functional main menu

---
+++ SteamPunkStealth/Assets/UiStuff/SliderValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+    Text valueText;
+    void Start()
+    {
+        valueText = GetComponent<Text>();
+    }
+
+    public void textUpdate (float value)
+    {
+        valueText.text = Mathf.RoundToInt(value);
+    }
+}
