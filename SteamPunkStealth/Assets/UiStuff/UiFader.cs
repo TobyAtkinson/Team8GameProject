b90a4cb77fa289480e9e90f9207659468c46d3fc
Edvilio <57681033+Edvilio@users.noreply.github.com>
2020-03-04 17:42:35 +0000

    Added HP bar reacting to damage and fading in / out on button click

---
+++ SteamPunkStealth/Assets/UiStuff/UiFader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiFader : MonoBehaviour
+{
+
+    public CanvasGroup uiElement;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 0));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup canvasGroup, float startValue, float endValue, float lerpTime = 0.5f)
+    {
+        float timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(startValue, endValue, percentageComplete);
+
+            canvasGroup.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
