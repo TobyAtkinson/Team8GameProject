d95433a0a68040ee3063f58652c4782fef479a2c
AdrianTofan <adrianvasile.tofan@gmail.com>
2020-04-24 17:03:33 +0300

    +Doors close behind the player once they enter the arena.

---
+++ SteamPunkStealth/Assets/Scripts/blindAIscripts/TriggerDoors.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoors : MonoBehaviour
+{
+    GameObject door1;
+    GameObject door2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door1 = GameObject.Find("gate1");
+        door2 = GameObject.Find("gate2");
+    }
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            door1.transform.Rotate(0, 40, 0);
+            door2.transform.Rotate(0, -55, 0);
+            Destroy(this);
+        }
+    }
+}
